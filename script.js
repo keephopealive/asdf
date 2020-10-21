977b51930ce1795a21b1539858930da1f127ccb5
Speros <smisirlakis@gmail.com>
2020-10-20 17:47:26 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  console.log("Document is done loading...")
+
+  $("#addClassBtn").click(function(){
+    console.log("addClass Click Event Invoked...")
+    $("#addClassP").addClass("blue")
+
+  })
+
+
+  $("#slideToggleBtn").click(function(){
+    console.log("sideToggle Click Event Invoked...")
+    $("#slideToggleP")
+
+  })
+
+
+
+})
